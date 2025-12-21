1360a3bb1b0caeae43867615ea8e0e40c497e678
YumeXII <Imammahardika628@gmail.com>
2025-12-21 14:07:11 +0700

    improvement

---
+++ js/search-index.js
@@ -0,0 +1,40 @@
+// Static search index for site-wide suggestions
+// This file is loaded dynamically by `main.js` and merged into the runtime search index.
+window.__SITE_SEARCH_INDEX = [
+  {
+    "type": "article",
+    "title": "Kenapa Pendidikan Paket A, B dan C Tidak Bisa Ditempuh dalam 1 Tahun?",
+    "href": "pages/blog/artikel-1.html",
+    "excerpt": "Penjelasan mengapa Pendidikan Paket A, B, dan C tidak dapat diselesaikan dalam 1 tahun serta faktor-faktor yang mempengaruhi masa belajar."
+  },
+  {
+    "type": "article",
+    "title": "Cek Keaslian Ijazah Paket A, B dan C",
+    "href": "pages/blog/artikel-2.html",
+    "excerpt": "Panduan praktis untuk memastikan ijazah paket A/B/C asli: cek hologram, nomor seri, NPSN lembaga, NISN peserta, dan konfirmasi ke Dinas Pendidikan."
+  },
+  {
+    "type": "article",
+    "title": "Info Penting Seputar PKBM dan Pendidikan Kesetaraan",
+    "href": "pages/blog/artikel-3.html",
+    "excerpt": "Informasi penting tentang PKBM: pengertian, legalitas (NPSN, akreditasi, izin operasional), dan pertanyaan umum seputar pendidikan kesetaraan."
+  },
+  {
+    "type": "article",
+    "title": "APA ITU PAKET A | B | C ??",
+    "href": "pages/blog/artikel-4.html",
+    "excerpt": "Penjelasan singkat tentang Program Paket A (Setara SD), Paket B (Setara SMP), dan Paket C (Setara SMA)."
+  },
+  {
+    "type": "page",
+    "title": "Blog",
+    "href": "pages/blog.html",
+    "excerpt": "Artikel, panduan, dan berita seputar pendidikan kesetaraan."
+  },
+  {
+    "type": "page",
+    "title": "Visi dan Misi",
+    "href": "pages/visi-misi.html",
+    "excerpt": "Visi dan misi PKBM Bale Rumawat: mengembangkan kecakapan abad ke-21 berakar pada kearifan lokal."
+  }
+];
